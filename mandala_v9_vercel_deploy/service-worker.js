82f4236ada463ae58dbae3812e52f4eae2292275
ENDUSOEPE <44526787+ENDUSOEPE@users.noreply.github.com>
2026-08-27 22:50:03 +0900

    Update service-worker.js

--- mandala_v9_vercel_deploy/service-worker.js
+++ mandala_v9_vercel_deploy/service-worker.js
@@ -1,40 +1,9 @@
-const CACHE='mandala-levelup-v11';
+const CACHE='mandala-levelup-v11-1';
 const ASSETS=['./','./index.html','./config.js','./manifest.json'];
-
-self.addEventListener('install',e=>
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(c=>c.addAll(ASSETS))
-      .then(()=>self.skipWaiting())
-  )
-);
-
-self.addEventListener('activate',e=>
-  e.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(
-        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
-      ))
-      .then(()=>self.clients.claim())
-  )
-);
-
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  if(u.origin!==location.origin)return;
-
-  if(e.request.mode==='navigate'){
-    e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));
-    return;
-  }
-
-  e.respondWith(
-    fetch(e.request)
-      .then(r=>{
-        const copy=r.clone();
-        caches.open(CACHE).then(c=>c.put(e.request,copy));
-        return r;
-      })
-      .catch(()=>caches.match(e.request))
-  );
+  const u=new URL(e.request.url); if(u.origin!==location.origin)return;
+  if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));return;}
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));
 });
