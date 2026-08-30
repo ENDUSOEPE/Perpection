c0ed77e3238e1666c2a8050ca03ecd2dbb4c1def
ENDUSOEPE <44526787+ENDUSOEPE@users.noreply.github.com>
2026-08-30 21:42:31 +0900

    Update service-worker.js

--- mandala_v9_vercel_deploy/service-worker.js
+++ mandala_v9_vercel_deploy/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mandala-levelup-v11-4';
+const CACHE='mandala-levelup-v11-5';
 const ASSETS=['./','./index.html','./config.js','./manifest.json'];
 
 self.addEventListener('install',e=>
