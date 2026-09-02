aee7139ee9d668f722ca792f40fb4fd3f700b9c0
ENDUSOEPE <44526787+ENDUSOEPE@users.noreply.github.com>
2026-09-02 22:15:50 +0900

    Update service-worker.js

--- mandala_v9_vercel_deploy/service-worker.js
+++ mandala_v9_vercel_deploy/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mandala-levelup-v12-2';
+const CACHE='mandala-levelup-v13-0';
 const ASSETS=['./','./index.html','./config.js','./manifest.json'];
 
 self.addEventListener('install',event=>{
@@ -12,28 +12,17 @@ self.addEventListener('install',event=>{
 self.addEventListener('activate',event=>{
   event.waitUntil(
     caches.keys()
-      .then(keys=>Promise.all(
-        keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))
-      ))
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
       .then(()=>self.clients.claim())
   );
 });
 
 self.addEventListener('fetch',event=>{
   const url=new URL(event.request.url);
-
   if(url.origin!==location.origin)return;
 
   if(event.request.mode==='navigate'){
-    event.respondWith(
-      fetch(event.request)
-        .then(response=>{
-          const copy=response.clone();
-          caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
-          return response;
-        })
-        .catch(()=>caches.match('./index.html'))
-    );
+    event.respondWith(fetch(event.request).catch(()=>caches.match('./index.html')));
     return;
   }
 
