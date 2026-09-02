28be5844a99d59121f462d6caa48c6f160021318
ENDUSOEPE <44526787+ENDUSOEPE@users.noreply.github.com>
2026-09-02 22:46:12 +0900

    Update service-worker.js

--- mandala_v9_vercel_deploy/service-worker.js
+++ mandala_v9_vercel_deploy/service-worker.js
@@ -1,38 +1,9 @@
-const CACHE='mandala-levelup-v13-2';
+const CACHE='mandala-levelup-v14-0';
 const ASSETS=['./','./index.html','./config.js','./manifest.json'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache=>cache.addAll(ASSETS))
-      .then(()=>self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
-      .then(()=>self.clients.claim())
-  );
-});
-
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
-  const url=new URL(event.request.url);
-  if(url.origin!==location.origin)return;
-
-  if(event.request.mode==='navigate'){
-    event.respondWith(fetch(event.request).catch(()=>caches.match('./index.html')));
-    return;
-  }
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-        return response;
-      })
-      .catch(()=>caches.match(event.request))
-  );
+ const url=new URL(event.request.url); if(url.origin!==location.origin)return;
+ if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).catch(()=>caches.match('./index.html')));return;}
+ event.respondWith(fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r;}).catch(()=>caches.match(event.request)));
 });
